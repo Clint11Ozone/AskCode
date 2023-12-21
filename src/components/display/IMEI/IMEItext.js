2ccf20e9627e9ddd74de8fa88e52ece1e295c07e
Souvik <souvikmandal210@gmail.com>
2023-12-22 00:18:13 +0530

    issue 5 fixed

--- src/components/display/IMEI/IMEItext.js
+++ src/components/display/IMEI/IMEItext.js
@@ -1,13 +1,14 @@
 import React from "react";
 // import IMEI from "/assets/illustrtations/IMEI.svg"
 
-const IconTextField = (image) => {
-    console.log(image);
+const IconTextField = ({IMEItext}) => {
+    console.log(IMEItext);
+    console.log(IMEItext?.IOS);
     var IMEI = "";
   return (
     <div className=" w-[375px] font-silkscreen pt-5 pb-2.5 flex flex-col text-center items-center">
       <form className="flex flex-row gap-[10px] w-[340px] border p-[20px] w-[340px] rounded-lg" >
-        <img src={image?.IMEI} alt="" />
+        <img src={IMEItext?.IOS} alt="" />
         <input type="text" placeholder="fill in your IMEI" />
       </form>
     </div>
