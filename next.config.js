94bf0ae3880f786df6db248abc2ffaf382b231b3
Clint <Siphiwe19375@gmail.com>
2024-03-28 09:56:46 +0200

    vercel test webhook

--- next.config.js
+++ next.config.js
@@ -14,6 +14,7 @@ module.exports = (phase, { defaultConfig }) => {
           "fs": false,
           "path": false,
           "os": false,
+          "net": false,
         }
       }
       return config
